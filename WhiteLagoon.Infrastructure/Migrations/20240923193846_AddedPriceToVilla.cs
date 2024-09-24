95ac3bd8e26542eb70de31942519877cc31766c9
ScottSmithGitHub <scott.smith1500@gmail.com>
2024-09-24 10:31:19 -0400

    Implemented toastr notifications

---
+++ WhiteLagoon.Infrastructure/Migrations/20240923193846_AddedPriceToVilla.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPriceToVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SqFt",
+                table: "Villas",
+                newName: "Sqft");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Villas",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Villas");
+
+            migrationBuilder.RenameColumn(
+                name: "Sqft",
+                table: "Villas",
+                newName: "SqFt");
+        }
+    }
+}
---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("description", "The description and name cannot exactly match.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                //_villaService.CreateVilla(obj);
+                TempData["success"] = "The villa has been created successfully.";
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            //Villa? obj = _villaService.GetVillaById(villaId);
+
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("description", "The description and name cannot exactly match.");
+            }
+
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+
+                //_villaService.UpdateVilla(obj);
+
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been updated successfully.";
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            //Villa? obj = _villaService.GetVillaById(villaId);
+
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been deleted successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["error"] = "Failed to delete the villa.";
+            return View();
+        }
+    }
+}
